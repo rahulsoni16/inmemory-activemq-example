dcb8d9415ea8e568d0183ca7430bae63866b16e2
Rahul Soni <rahul.soni16@gmail.com>
2018-03-19 12:09:20 +0530

    commit

---
+++ src/test/java/com/code/messaging/inmemoryactivemqexample/InmemoryActivemqExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.code.messaging.inmemoryactivemqexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InmemoryActivemqExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
